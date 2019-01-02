21d00259dc980844b5975c387adf444c02f9a336
Hernán Cote <hernan.cote@outlook.com>
2019-01-02 15:19:30 -0500

    Add CORS Policy

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ mongoose
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
